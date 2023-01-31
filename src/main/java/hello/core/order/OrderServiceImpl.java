4df2facc4cb6aed4cda886292966076708766aef
whddla <whddla8912@gmail.com>
2023-01-31 20:42:35 +0900

    주문 서비스 구현체

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+// 주문 서비스 구현체
 @Component
 @RequiredArgsConstructor // 생성자를 만들어줌
 public class OrderServiceImpl implements OrderService {
