6778b86cceaab51d8df1d1452f5cdf1ee242278a
whddla <whddla8912@gmail.com>
2023-01-31 21:13:39 +0900

    애노테이션 생성

--- src/main/java/hello/core/annotation/MainDiscountPolicy.java
+++ src/main/java/hello/core/annotation/MainDiscountPolicy.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.lang.annotation.*;
 
+// 애노테이션 만들기
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
